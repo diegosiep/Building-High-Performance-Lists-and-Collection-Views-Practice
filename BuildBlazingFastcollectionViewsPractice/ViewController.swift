87462ef8fc34c198054b46a8e41fc787c800d45f
Diego Sierra <diegosiep@icloud.com>
2023-10-03 12:53:14 -0600

    Xcode project files creation

---
+++ BuildBlazingFastcollectionViewsPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BuildBlazingFastcollectionViewsPractice
+//
+//  Created by Diego Sierra on 03/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
